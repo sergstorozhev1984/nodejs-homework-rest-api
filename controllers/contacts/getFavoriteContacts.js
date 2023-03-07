300a5ddbf479b63d290eab337cf0955cdd2b2cd5
Sergey <92057970+sergstorozhev1984@users.noreply.github.com>
2023-03-07 13:07:22 +0200

    add favorite owner

--- controllers/contacts/getFavoriteContacts.js
+++ controllers/contacts/getFavoriteContacts.js
@@ -2,12 +2,14 @@ const { Contact } = require("../../models/contact");
 
 const getFavoriteContacts = async (req, res) => {
   const favorite = req.query.favorite;
-  const filter = {};
+  const owner = req.user._id;
+  const filter = { owner };
+
   if (favorite !== undefined) {
     filter.favorite = favorite;
-    }
-    const contacts = await Contact.find(filter);
-    res.json(contacts);
+  }
+  const contacts = await Contact.find(filter);
+  res.json(contacts);
 };
 
 module.exports = getFavoriteContacts;

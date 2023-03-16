3c85c171a801291bce8155dd88a943a7ca2bfa74
Sergey <92057970+sergstorozhev1984@users.noreply.github.com>
2023-03-16 12:40:20 +0200

    hw-5 full

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
-require("dotenv").config()
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+require("dotenv").config();
+
 
 const contactsRouter = require('./routes/api/contacts');
 const usersRouter = require("./routes/api/users");
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-app.use(logger(formatsLogger))
-app.use(cors()) 
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors()) ;
+app.use(express.json());
+app.use(express.static('public'));
 
 app.use('/api/contacts', contactsRouter);
 app.use('/api/users', usersRouter);
--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,7 @@ const {ctrlWrapper} = require("../../helpers");
 const {schemas} = require("../../models/user");
 
 const controllers = require("../../controllers/users");
+const upload = require("../../middlewares/upload");
 
 const router = express.Router();
 
@@ -18,7 +19,9 @@ router.get("/logout", authenticate, ctrlWrapper(controllers.logout));
 
 router.get("/current", authenticate, ctrlWrapper(controllers.getCurrent));
 
-router.patch("/:id/subscription", authenticate, validateBody(schemas.subscriptionSchema), ctrlWrapper(controllers.updateSubscription))
+router.patch("/:id/subscription", authenticate, validateBody(schemas.subscriptionSchema), ctrlWrapper(controllers.updateSubscription));
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrlWrapper(controllers.updateAvatar));
 
 module.exports = router;
 
